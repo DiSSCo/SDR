c1e7c327a81b31c17b2301384cec8440912f0185
Paul Brack <paul.brack@manchester.ac.uk>
2021-07-26 13:50:10 +0100

    Wire together options and json

--- galaxy-workflow/tools/create-opends/create_opends.rb
+++ galaxy-workflow/tools/create-opends/create_opends.rb
@@ -3,46 +3,37 @@ require 'optparse'
 
 options = {}
 OptionParser.new do |opts|
-  opts.on("--catalog_number", "Catalog number") do |catalog_number|
+  opts.on("--catalog_number=CATALOG_NUMBER", "Catalog number") do |catalog_number|
     options[:catalog_number] = catalog_number
   end
-    opts.on("--image_license", "Image license") do |image_license|
+    opts.on("--image_license=IMAGE_LICENSE", "Image license") do |image_license|
     options[:image_license] = image_license
   end
-    opts.on("--image_uri", "Image URI") do |image_uri|
+    opts.on('--image_uri=IMAGE_URI', "Image URI") do |image_uri|
     options[:image_uri] = image_uri
   end
-    opts.on("--object_type", "Object Type") do |object_type|
+    opts.on("--object_type=OBJECT_TYPE", "Object Type") do |object_type|
     options[:object_type] = object_type
   end
-    opts.on("--rights_holder", "Rights Holder") do |rights_holder|
+    opts.on("--rights_holder=RIGHTS_HOLDER", "Rights Holder") do |rights_holder|
     options[:rights_holder] = rights_holder
   end
-    opts.on("--registered_institution_url", "Registered Institution URL number") do |registered_institution_url|
+    opts.on("--registered_institution_url=INSTITUTIONAL_URL", "Registered Institution URL number") do |registered_institution_url|
     options[:registered_institution_url] = registered_institution_url
   end
-    opts.on("--higher_classification", "Higher Classification") do |higher_classification|
+    opts.on("--higher_classification=HIGHER_CLASSIFICATION", "Higher Classification") do |higher_classification|
     options[:higher_classification] = higher_classification
   end
-    opts.on("--person_name", "Person Name") do |person_name|
+    opts.on("--person_name[=PERSON_NAME]", "Person Name") do |person_name|
     options[:person_name] = person_name
   end
-    opts.on("--person_identifier", "Person Identifier") do |person_identifier|
+    opts.on("--person_identifier[=PERSON_IDENTIFIER]", "Person Identifier") do |person_identifier|
     options[:person_identifier] = person_identifier
   end
-    opts.on("--output", "Output") do |output|
+    opts.on("--output[=OUTPUT]", "Output") do |output|
     options[:output] = output
   end
 end.parse!
-
-#Add reqd args here
-raise OptionParser::MissingArgument if options[:catalog_number].nil?
-raise OptionParser::MissingArgument if options[:image_license].nil?
-raise OptionParser::MissingArgument if options[:image_uri].nil?
-raise OptionParser::MissingArgument if options[:object_type].nil?
-raise OptionParser::MissingArgument if options[:rights_holder].nil?
-raise OptionParser::MissingArgument if options[:registered_institution_url].nil?
-raise OptionParser::MissingArgument if options[:higher_classification].nil?
 
 open_DS = Hash.new
 
@@ -67,3 +58,4 @@ if(!options[:person_identifier].nil?)
 end
 
 puts open_DS.to_json
+
