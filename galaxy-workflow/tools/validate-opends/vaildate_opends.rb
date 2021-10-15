6995c2e94cc484201a22f35708945b62f61a8f9a
Paul Brack <paul.brack@manchester.ac.uk>
2021-10-15 22:46:10 +0100

    Add JSON validator. Change material type to dropdown

---
+++ galaxy-workflow/tools/validate-opends/vaildate_opends.rb
@@ -0,0 +1,30 @@
+require 'json'
+require 'optparse'
+require 'json-schema'
+
+def valid_json?(json)
+  JSON.parse(json)
+  return true
+rescue JSON::ParserError => e
+  return false
+end
+
+options = {}
+OptionParser.new do |opts|
+  opts.on("--opends=OPENDS", "openDS") do |open_ds|
+    options[:open_ds] = open_ds
+  end
+
+end.parse!
+
+#first check that we have valid JSON
+json = File.read(options[:open_ds])
+valid = valid_json?(json)
+
+#now check against schema
+schema = File.read("opends_schema.json")
+if(valid) then
+  valid = JSON::Validator.validate(schema, json)
+end
+
+puts valid
