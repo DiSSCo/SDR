cbb2f6c60acc752165d1bea32ac71ff3000ab8a0
Paul Brack <paul.brack@manchester.ac.uk>
2021-10-20 23:13:25 +0100

    Improve deployment script

--- deploy_tools.sh
+++ deploy_tools.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 function strEcho(){
     local exp=$1;
-	echo "\033[1;32;41m${exp}\e[0m";
+	echo -e "\033[1;32;41m${exp}\e[0m";
+	echo "";
 }
-
 
 strEcho "Updating repository"
 git pull
@@ -10,7 +12,7 @@ git pull
 strEcho "Stopping Galaxy server"
 sh ~/galaxy/run.sh --stop-daemon
 
-strEcho "copying tool scripts"
+strEcho "Copying tool scripts"
 cp -r ~/SDR/galaxy-workflow/tools/* ~/galaxy/tools/sdr
 
 #remove generated tool panel config file - this is regenerated at Galaxy boot time
