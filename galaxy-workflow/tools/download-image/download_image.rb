68e186efddc3e0a55ace43c04c4d54bf1158dc9e
Paul Brack <paul.brack@manchester.ac.uk>
2021-10-20 18:33:10 +0100

    Update image download script. Revert openDS creation to previous version (changed in error)

--- galaxy-workflow/tools/download-image/download_image.rb
+++ galaxy-workflow/tools/download-image/download_image.rb
@@ -1,4 +1,13 @@
-require 'json'
+require 'net/http'
 require 'optparse' #TODO - implement options parser to make args optional
 
-image_URI = 'http://mediaphoto.mnhn.fr/media/1427463506459UAeHPJw9jXoET1sF'
+image_URI = 'http://google.com'
+local_folder = ''
+file_name = 'test.name'
+
+concat_file_name = File.join(local_folder, file_name)
+
+File.write(concat_file_name, Net::HTTP.get(URI.parse(image_URI)))
+
+sleep(5000)
+concat_file_name
