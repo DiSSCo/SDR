c5070819bd01c28f652c4127027d602278e8ef68
Paul Brack <paul.brack@manchester.ac.uk>
2021-10-21 02:52:54 +0100

    Amend deployment script to copy additional config

--- deploy_tools.sh
+++ deploy_tools.sh
@@ -21,12 +21,16 @@ rsync -av ~/galaxy/tools/sdr ~/galaxy/AUTO_DEPLOY_BACKUP/${TIMESTRING}/tools
 strEcho "Copying tool scripts"
 cp -rf ~/SDR/galaxy-workflow/tools/* ~/galaxy/tools/sdr
 
-#remove generated tool panel config file - this is regenerated at Galaxy boot time
+
 strEcho "Updating configuration"
-rm ~/galaxy/config/integrated_tool_panel.xml 
+rm ~/galaxy/config/integrated_tool_panel.xml #remove generated tool panel config file - this is regenerated at Galaxy boot time
 
 #copy tool panel config files
 cp -f ~/SDR/galaxy-workflow/config/tool_conf.xml ~/galaxy/config/tool_conf.xml
+cp -f ~/SDR/galaxy-workflow/config/sdr_tool_conf.xml ~/galaxy/config/sdr_tool_conf.xml
+
+#copy main server config
+cp -f ~/SDR/galaxy-workflow/config/galaxy.yml ~/galaxy/config/galaxy.yml
 
 #copy data type config files
 
