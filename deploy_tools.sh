d150ca88f1c54685709d3c2d4f7ca2c118238704
Paul Brack <paul.brack@manchester.ac.uk>
2021-10-21 03:53:07 +0100

    Amend validator

--- deploy_tools.sh
+++ deploy_tools.sh
@@ -18,9 +18,8 @@ mkdir -p ~/galaxy/AUTO_DEPLOY_BACKUP/${TIMESTRING}/tools
 rsync -av --exclude 'config/plugins' ~/galaxy/config ~/galaxy/AUTO_DEPLOY_BACKUP/${TIMESTRING} #plugins directory is 1.5GB so don't back it up
 rsync -av ~/galaxy/tools/sdr ~/galaxy/AUTO_DEPLOY_BACKUP/${TIMESTRING}/tools
 
-strEcho "Copying tool scripts"
+strEcho "Copying tools"
 cp -rf ~/SDR/galaxy-workflow/tools/* ~/galaxy/tools/sdr
-
 
 strEcho "Updating configuration"
 rm ~/galaxy/config/integrated_tool_panel.xml #remove generated tool panel config file - this is regenerated at Galaxy boot time
