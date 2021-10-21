40902d81bb4cccba8af0c21c770018c0f1b0f09b
Paul Brack <paul.brack@manchester.ac.uk>
2021-10-21 23:09:58 +0100

    Amend download image tool

--- galaxy-workflow/tools/download-image/download_image.rb
+++ galaxy-workflow/tools/download-image/download_image.rb
@@ -1,12 +1,21 @@
 require 'net/http'
-require 'optparse' #TODO - implement options parser to make args optional
+require 'optparse' 
 
-image_URI = 'http://google.com'
-local_folder = ''
-file_name = 'test.name'
+options = {}
+OptionParser.new do |opts|
+  opts.on("--image_URI=IMAGEURI") do |image_URI|
+    options[:image_URI] = image_URI
+  end
+  opts.on("--local_folder=LOCALFOLDER") do |local_folder|
+    options[:local_folder] = local_folder
+  end
+    opts.on("--file_name=FILENAME") do |file_name|
+    options[:file_name] = file_name
+  end
+end.parse!
 
-concat_file_name = File.join(local_folder, file_name)
+concat_file_name = File.join(options[:local_folder], options[:file_name])
 
-File.write(concat_file_name, Net::HTTP.get(URI.parse(image_URI)))
+File.write(concat_file_name, Net::HTTP.get(URI.parse(options[:image_URI])))
 
 concat_file_name
