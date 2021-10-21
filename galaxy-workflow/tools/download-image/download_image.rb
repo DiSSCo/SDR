79d34d531ca4502b057dbacca611fa6dbf68d0b6
Paul Brack <paul.brack@manchester.ac.uk>
2021-10-21 23:37:38 +0100

    Amend download image tool

--- galaxy-workflow/tools/download-image/download_image.rb
+++ galaxy-workflow/tools/download-image/download_image.rb
@@ -6,15 +6,14 @@ OptionParser.new do |opts|
   opts.on("--image_uri=IMAGEURI") do |image_uri|
     options[:image_uri] = image_uri
   end
-  opts.on("--local_folder=LOCALFOLDER") do |local_folder|
-    options[:local_folder] = local_folder
-  end
     opts.on("--file_name=FILENAME") do |file_name|
     options[:file_name] = file_name
   end
 end.parse!
 
-concat_file_name = File.join(options[:local_folder], options[:file_name])
+local_folder = '/home/paulb1/tempImages'
+
+concat_file_name = File.join(local_folder, options[:file_name])
 
 File.write(concat_file_name, Net::HTTP.get(URI.parse(options[:image_uri])))
 
