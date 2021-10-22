4b68c3db5789f677dbc70d93c0615db86abc330e
Paul Brack <paul.brack@manchester.ac.uk>
2021-10-22 02:24:33 +0100

    Amend download image tool

--- galaxy-workflow/tools/download-image/download_image.rb
+++ galaxy-workflow/tools/download-image/download_image.rb
@@ -9,8 +9,9 @@ OptionParser.new do |opts|
   end
 end.parse!
 
+ext = options[:image_uri].split('.')[-1]
 local_folder = '/home/paulb1/tempImages'
-file_name = "#{SecureRandom.uuid}.jpg"
+file_name = "#{SecureRandom.uuid}.#{ext}"
 
 concat_file_name = File.join(local_folder, file_name)
 
