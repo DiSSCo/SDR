1cd9b1f24c3bc2746746537cce3ca06a77025c25
Paul Brack <paul.brack@manchester.ac.uk>
2021-10-25 13:13:54 +0100

    Add metadata to image

--- galaxy-workflow/tools/download-image/download_image.rb
+++ galaxy-workflow/tools/download-image/download_image.rb
@@ -1,11 +1,13 @@
 require 'net/http'
 require 'optparse' 
 require 'securerandom'
+require 'json'
+require 'exifr'
 
 options = {}
 OptionParser.new do |opts|
-  opts.on("--image_uri=IMAGEURI") do |image_uri|
-    options[:image_uri] = image_uri
+  opts.on("--opends=OPENDS") do |open_ds|
+    options[:open_ds] = open_ds
   end
 end.parse!
 
@@ -17,4 +19,19 @@ concat_file_name = File.join(local_folder, file_name)
 
 File.write(concat_file_name, Net::HTTP.get(URI.parse(options[:image_uri])))
 
-concat_file_name
+open_ds=JSON.parse(options[:open_ds])
+
+#####read file metadata
+EXIFR::JPEG.new('IMG_6841.JPG').width                  
+
+
+#####create json payload
+open_DS[:images][:availableImages][0][:imageWidth] = EXIFR::JPEG.new('IMG_6841.JPG').width               
+
+
+
+#####put json payload into opends
+
+
+
+opends = options[:open_ds] + "text"
