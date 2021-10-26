0587a7e8110fa53d24676e72c86616b43a4744ea
Paul Brack <paul.brack@manchester.ac.uk>
2021-10-26 16:32:36 +0100

    Download image to local temp dir

--- galaxy-workflow/tools/download-image/download_image.rb
+++ galaxy-workflow/tools/download-image/download_image.rb
@@ -15,20 +15,22 @@ end.parse!
 open_ds = JSON.parse(File.read(options[:open_ds]))
 
 ##### Create a file name
+local_folder = '/home/paulb1/tempImages'
 image_uri = open_ds["images"]["availableImages"][0]["source"]
 ext = FastImage.type(image_uri).to_s #get file type by mime type
 file_name = "#{SecureRandom.uuid}.#{ext}" 
-File.write(file_name, Net::HTTP.get(URI.parse(image_uri)))
+concat_file_name = File.join(local_folder, file_name)
+File.write(concat_file_name, Net::HTTP.get(URI.parse(image_uri)))
 
 ##### Read file metadata
-size_array = FastImage.size(file_name)
+size_array = FastImage.size(concat_file_name)
 open_ds["payloads"] = Hash.new
 open_ds["payloads"]["name"] = 'original image'
-open_ds["payloads"]["filename"] = file_name
+open_ds["payloads"]["filename"] = concat_file_name
 open_ds["payloads"]["width"] = size_array[0]
 open_ds["payloads"]["height"] = size_array[1]
 open_ds["payloads"]["mediaType"] = "image/#{ext}"
-open_ds["payloads"]["size n"] = File.size(file_name)
+open_ds["payloads"]["size n"] = File.size(concat_file_name)
 
 
 puts open_ds.to_json
