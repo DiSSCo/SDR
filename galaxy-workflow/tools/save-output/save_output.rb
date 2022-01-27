18d0ee63dc832e53b6c000cf36d8de3f67bd5ff4
Paul Brack <paul.brack@manchester.ac.uk>
2022-01-27 18:14:19 +0000

    Add results folder as static path. Add save results tool. Add download image functionality to create  opends tool

--- galaxy-workflow/tools/save-output/save_output.rb
+++ galaxy-workflow/tools/save-output/save_output.rb
@@ -1,8 +1,5 @@
 require 'json'
 require 'optparse'
-require 'net/http'
-require 'securerandom'
-require 'fastimage'
 
 options = {}
 OptionParser.new do |opts|
@@ -56,25 +53,5 @@ end
 if(!options[:person_identifier].nil?)
   open_DS[:person_identifier] = options[:person_identifier]
 end
-
-
-
-##### Create a file name
-local_folder = ENV["SDR_IMAGE_DOWNLOAD_DIRECTORY"]
-image_uri = options[:image_uri]
-ext = FastImage.type(image_uri).to_s #get file type by mime type
-file_name = "#{SecureRandom.uuid}.#{ext}" 
-concat_file_name = File.join(local_folder, file_name)
-File.write(concat_file_name, Net::HTTP.get(URI.parse(image_uri)))
-
-##### Read file metadata
-size_array = FastImage.size(concat_file_name)
-open_ds["payloads"] = Hash.new
-open_ds["payloads"]["name"] = 'original image'
-open_ds["payloads"]["filename"] = file_name
-open_ds["payloads"]["width"] = size_array[0]
-open_ds["payloads"]["height"] = size_array[1]
-open_ds["payloads"]["mediaType"] = "image/#{ext}"
-open_ds["payloads"]["size n"] = File.size(concat_file_name)
 
 puts open_DS.to_json
