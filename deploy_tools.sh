7e5967d31a8db06adaffb8c7ea23953586dc5294
Paul Brack <paul.brack@manchester.ac.uk>
2022-02-02 00:36:33 +0000

    Change Galaxy location in deploy script

--- deploy_tools.sh
+++ deploy_tools.sh
@@ -10,42 +10,42 @@ strEcho "Updating repository"
 git pull
 
 strEcho "Stopping Galaxy server"
-sh ~/galaxy/run.sh --stop-daemon
+sh /var/www/html/galaxy/run.sh --stop-daemon
 
 strEcho "Backing up config and tool folders"
 TIMESTRING=$(date +%Y-%m-%dT%H%M%S)
-mkdir -p ~/galaxy/AUTO_DEPLOY_BACKUP/${TIMESTRING}/tools
-rsync -av --exclude 'config/plugins' ~/galaxy/config ~/galaxy/AUTO_DEPLOY_BACKUP/${TIMESTRING} #plugins directory is 1.5GB so don't back it up
-rsync -av ~/galaxy/tools/sdr ~/galaxy/AUTO_DEPLOY_BACKUP/${TIMESTRING}/tools
+mkdir -p /var/www/html/galaxy/AUTO_DEPLOY_BACKUP/${TIMESTRING}/tools
+rsync -av --exclude 'config/plugins' /var/www/html/galaxy/config /var/www/html/galaxy/AUTO_DEPLOY_BACKUP/${TIMESTRING} #plugins directory is 1.5GB so don't back it up
+rsync -av /var/www/html/galaxy/tools/sdr /var/www/html/galaxy/AUTO_DEPLOY_BACKUP/${TIMESTRING}/tools
 
 strEcho "Copying tools"
-cp -rf ~/SDR/galaxy-workflow/tools/* ~/galaxy/tools/sdr
+cp -rf ~/SDR/galaxy-workflow/tools/* /var/www/html/galaxy/tools/sdr
 
 strEcho "Updating configuration"
-rm ~/galaxy/config/integrated_tool_panel.xml #remove generated tool panel config file - this is regenerated at Galaxy boot time
+rm /var/www/html/galaxy/config/integrated_tool_panel.xml #remove generated tool panel config file - this is regenerated at Galaxy boot time
 
 #copy tool panel config files
-cp -f ~/SDR/galaxy-workflow/config/tool_conf.xml ~/galaxy/config/tool_conf.xml
-cp -f ~/SDR/galaxy-workflow/config/sdr_tool_conf.xml ~/galaxy/config/sdr_tool_conf.xml
-cp -f ~/SDR/galaxy-workflow/config/job_conf.xml ~/galaxy/config/job_conf.xml
-cp -f ~/SDR/galaxy-workflow/config/local_env.sh ~/galaxy/config/local_env.sh
-cp -f ~/SDR/galaxy-workflow/config/opends-schema.json ~/galaxy/config/opends-schema.json
+cp -f ~/SDR/galaxy-workflow/config/tool_conf.xml /var/www/html/galaxy/config/tool_conf.xml
+cp -f ~/SDR/galaxy-workflow/config/sdr_tool_conf.xml /var/www/html/galaxy/config/sdr_tool_conf.xml
+cp -f ~/SDR/galaxy-workflow/config/job_conf.xml /var/www/html/galaxy/config/job_conf.xml
+cp -f ~/SDR/galaxy-workflow/config/local_env.sh /var/www/html/galaxy/config/local_env.sh
+cp -f ~/SDR/galaxy-workflow/config/opends-schema.json /var/www/html/galaxy/config/opends-schema.json
 
 #copy main server config
-cp -f ~/SDR/galaxy-workflow/config/galaxy.yml ~/galaxy/config/galaxy.yml
+cp -f ~/SDR/galaxy-workflow/config/galaxy.yml /var/www/html/galaxy/config/galaxy.yml
 
 #copy data type files
-cp -f ~/SDR/galaxy-workflow/config/datatypes_conf.xml ~/galaxy/config/datatypes_conf.xml
+cp -f ~/SDR/galaxy-workflow/config/datatypes_conf.xml /var/www/html/galaxy/config/datatypes_conf.xml
 
 #copy visualisations
 strEcho "Updating visualisations"
-cp -rf ~/SDR/galaxy-workflow/visualisations/roi ~/galaxy/config/plugins/visualizations
+cp -rf ~/SDR/galaxy-workflow/visualisations/roi /var/www/html/galaxy/config/plugins/visualizations
 
 #copy static content
 strEcho "Updating statics"
-cp -rf ~/SDR/galaxy-workflow/static/* ~/galaxy/static/
+cp -rf ~/SDR/galaxy-workflow/static/* /var/www/html/galaxy/static/
 
 #copy data type config files
 
 strEcho "Restarting Galaxy server"
-sh ~/galaxy/run.sh --daemon
+sh /var/www/html/galaxy/run.sh --daemon
