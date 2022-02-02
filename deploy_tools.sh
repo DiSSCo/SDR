bc5f69addde96dfc8c67c343e3205ed47e8ed40c
Paul Brack <paul.brack@manchester.ac.uk>
2022-02-02 00:49:18 +0000

    Change location of git repo in deploy script

--- deploy_tools.sh
+++ deploy_tools.sh
@@ -19,31 +19,31 @@ rsync -av --exclude 'config/plugins' /var/www/html/galaxy/config /var/www/html/g
 rsync -av /var/www/html/galaxy/tools/sdr /var/www/html/galaxy/AUTO_DEPLOY_BACKUP/${TIMESTRING}/tools
 
 strEcho "Copying tools"
-cp -rf ~/SDR/galaxy-workflow/tools/* /var/www/html/galaxy/tools/sdr
+cp -rf /home/paulb1/SDR/galaxy-workflow/tools/* /var/www/html/galaxy/tools/sdr
 
 strEcho "Updating configuration"
 rm /var/www/html/galaxy/config/integrated_tool_panel.xml #remove generated tool panel config file - this is regenerated at Galaxy boot time
 
 #copy tool panel config files
-cp -f ~/SDR/galaxy-workflow/config/tool_conf.xml /var/www/html/galaxy/config/tool_conf.xml
-cp -f ~/SDR/galaxy-workflow/config/sdr_tool_conf.xml /var/www/html/galaxy/config/sdr_tool_conf.xml
-cp -f ~/SDR/galaxy-workflow/config/job_conf.xml /var/www/html/galaxy/config/job_conf.xml
-cp -f ~/SDR/galaxy-workflow/config/local_env.sh /var/www/html/galaxy/config/local_env.sh
-cp -f ~/SDR/galaxy-workflow/config/opends-schema.json /var/www/html/galaxy/config/opends-schema.json
+cp -f fill:hsl(6, 80%, 50%);/galaxy-workflow/config/tool_conf.xml /var/www/html/galaxy/config/tool_conf.xml
+cp -f /home/paulb1/SDR/galaxy-workflow/config/sdr_tool_conf.xml /var/www/html/galaxy/config/sdr_tool_conf.xml
+cp -f /home/paulb1/SDR/galaxy-workflow/config/job_conf.xml /var/www/html/galaxy/config/job_conf.xml
+cp -f /home/paulb1/SDR/galaxy-workflow/config/local_env.sh /var/www/html/galaxy/config/local_env.sh
+cp -f /home/paulb1/SDR/galaxy-workflow/config/opends-schema.json /var/www/html/galaxy/config/opends-schema.json
 
 #copy main server config
-cp -f ~/SDR/galaxy-workflow/config/galaxy.yml /var/www/html/galaxy/config/galaxy.yml
+cp -f /home/paulb1/SDR/galaxy-workflow/config/galaxy.yml /var/www/html/galaxy/config/galaxy.yml
 
 #copy data type files
-cp -f ~/SDR/galaxy-workflow/config/datatypes_conf.xml /var/www/html/galaxy/config/datatypes_conf.xml
+cp -f /home/paulb1/SDR/galaxy-workflow/config/datatypes_conf.xml /var/www/html/galaxy/config/datatypes_conf.xml
 
 #copy visualisations
 strEcho "Updating visualisations"
-cp -rf ~/SDR/galaxy-workflow/visualisations/roi /var/www/html/galaxy/config/plugins/visualizations
+cp -rf /home/paulb1/SDR/galaxy-workflow/visualisations/roi /var/www/html/galaxy/config/plugins/visualizations
 
 #copy static content
 strEcho "Updating statics"
-cp -rf ~/SDR/galaxy-workflow/static/* /var/www/html/galaxy/static/
+cp -rf /home/paulb1/SDR/galaxy-workflow/static/* /var/www/html/galaxy/static/
 
 #copy data type config files
 
